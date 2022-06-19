591f72927d0ee6f2ea62de99fc8cc4399fc03ef0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 10:48:47 +0000

    Called toggletask function in Tasks component

--- src/App.js
+++ src/App.js
@@ -20,20 +20,24 @@ function App() {
         id: 3,
         text : "Food Shopping",
         day : "Feb 5th at 2:30pm",
-        remainder: false,
+        reminder: false,
       }
     ]);
 
-    //Delete Task
+    //DELETE TASK
     const deleteTask = (id) =>{
       setTasks(tasks.filter((task) => task.id !== id))
     }
 
+    //TOGGLE TASK
+    const toggleTask = (id) =>{
+      setTasks(tasks.map((task) => task.id === id ?{...task, reminder: !task.reminder} : tasks ))
+    }
 
   return (
     <div className="container">
      <Header/>
-     {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask}/> : <h3>There is no task to show</h3>}
+     {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle= {toggleTask}/> : <h3>There is no task to show</h3>}
     </div>
   );
 }
