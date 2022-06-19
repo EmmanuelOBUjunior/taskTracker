2d96c7b0cfb03c7934974f0630f37ffb0ba33710
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 12:48:04 +0000

    Completed UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({title, onAdd, showAdd}) => {
   return (
 	<header className='header'>
 		<h1>{title}</h1>
-		<Button color= {showAdd ? 'green' : 'red'} onClick={onAdd}/>
+		<Button color= {showAdd ? 'red' : 'steelblue'} text = {showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
 	</header>
   )
 }
