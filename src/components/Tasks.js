e17daa4d97ffe7a5fad8c6305bb5f5fa62cbbab4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 08:51:10 +0000

    Modified Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,3 @@
-const tasks = [
-	{
-		id: 1,
-		text : "Doctor's Appointment",
-		day : "Feb 5th at 2:30pm",
-		remainder: true
-	},
-	{
-		id: 2,
-		text : "Meeting at School",
-		day : "Feb 6th at 1:30pm",
-		remainder: true
-	},
-	{
-		id: 3,
-		text : "Food Shopping",
-		day : "Feb 5th at 2:30pm",
-		remainder: false,
-	}
-]
-
 const Tasks = () => {
   return (
 	<>
