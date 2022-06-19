93987211b8d55c8affd6187602986804624d75b9
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 08:23:28 +0000

    Object of tasks add(Task.js)

---
+++ src/components/Tasks.js
@@ -0,0 +1,27 @@
+const task = [
+	{
+		id: 1,
+		text : "Doctor's Appointment",
+		day : "Feb 5th at 2:30pm",
+		remainder: true
+	},
+	{
+		id: 2,
+		text : "Meeting at School",
+		day : "Feb 6th at 1:30pm",
+		remainder: true
+	},
+	{
+		id: 3,
+		text : "Food Shopping",
+		day : "Feb 5th at 2:30pm",
+		remainder: false,
+	}
+]
+const Tasks = () => {
+  return (
+	<div>Tasks</div>
+  )
+}
+
+export default Tasks
