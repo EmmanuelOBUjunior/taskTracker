862207eccc69837ec5808cac226a4f3849f0e071
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 09:38:53 +0000

    Implemented delete icon

--- src/App.js
+++ src/App.js
@@ -23,10 +23,17 @@ function App() {
         remainder: false,
       }
     ]);
+
+    //Delete Task
+    const deleteTask = (id) =>{
+      console.log("Deleted task with id", id);
+    }
+
+
   return (
     <div className="container">
      <Header/>
-     <Tasks tasks = {tasks}/>
+     <Tasks tasks = {tasks} onDelete = {deleteTask}/>
     </div>
   );
 }
