907741f20678890fa313611c8e1414515ff873cc
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 13:42:37 +0000

    Add to server(POST)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   const [tasks, setTasks] = useState([]);
   useEffect(()=>{
     const getTasks = async () =>{
-      const taskFromServer = await fetchTasks();
-      setTasks(taskFromServer)
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
     }
 
     getTasks();
@@ -21,19 +21,36 @@ function App() {
       const res = await fetch('http://localhost:5000/tasks')
       const data = await res.json()
       
+      //console.log(data)
       return data
     }
 
     //ADD TASK
-    const addTask = (task) =>{
-      const id = Math.floor(Math.random() * 10000 +1 )
-      const newTask = {id, ...task}
+    const addTask = async(task) =>{
+      const res = await fetch('http://localhost:5000/tasks',{
+        method: "POST",
+        headers:{
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+      
+      const data = await res.json()
 
-      setTasks([...tasks, newTask])
+      setTasks([...tasks, data])
+      
+      // const id = Math.floor(Math.random() * 10000 +1 )
+      // const newTask = {id, ...task}
+
+      // setTasks([...tasks, newTask])
     }
 
     //DELETE TASK
-    const deleteTask = (id) =>{
+    const deleteTask = async (id) =>{
+      await fetch(`http://localhost:5000/tasks/${id}`,{
+          method: "DELETE",
+        })
+
      setTasks(tasks.filter((task) => task.id !== id))
     }
 
