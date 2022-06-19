9c7a3044d1ad392be364d3218db341172c736cf1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-19 11:37:20 +0000

    Implemented setState for the various inputs

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -9,15 +9,15 @@ const AddTask = () => {
 		<div className="add-form">
 			<div className="form-control">
 				<label htmlFor="task">Task</label>
-				<input id = "task" type="text" placeholder="Add Task"/>
+				<input id = "task" type="text" placeholder="Add Task" value={text} onChange = {(e) => setText(e.target.value)}/>
 			</div>
 			<div className="form-control">
 				<label htmlFor="day_time">Set Day & Time</label>
-				<input id = "day_time" type="text" placeholder="Set Day and Time"/>
+				<input id = "day_time" type="text" placeholder="Set Day and Time" value={day} onChange = {(e) => setDay(e.target.value)}/>
 			</div>
 			<div className="form-control form-control-check">
-				<label htmlFor="reminder">Set Reminder</label>
-				<input id = "reminder" type="checkbox"/>
+				<label >Set Reminder</label>
+				<input type="checkbox" value={reminder} onChange = {(e) => setReminder(e.currentTarget.checked)}/>
 			</div>
 			<input type="submit" className="btn btn-block" value="Save Task"/>
 		</div>
